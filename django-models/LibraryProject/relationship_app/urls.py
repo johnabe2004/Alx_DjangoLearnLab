e7d49dd6e9cfbfdfde4c4ab41e03e16fb5d702aa
Yohannes Abebe <johnabe2004@gmail.com>
2026-03-01 00:20:50 +0300

    Role-Based Access is created

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from . import views
+from .views import admin_view, librarian_view, member_view
 
 urlpatterns = [
-    # ... existing book and library paths ...
-    
-    # Authentication URLs
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),
+    # ... existing URLs ...
+    path('admin_view/', admin_view, name='admin_view'),
+    path('librarian_view/', librarian_view, name='librarian_view'),
+    path('member_view/', member_view, name='member_view'),
 ]
